af5fc828c92ba0c48a07ac7a890277acb406d573
vitaliibobylov <bobylevvitalyanatolievich@gmail.com>
2025-10-30 10:53:34 +0200

    app

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
-const message = 'Hello world';
+import express from 'express';
 
-console.log(message);
+const app = express();
+
+const PORT = 3000;
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({"message": "Retrieved all notes"});
+});
+
+app.get("/notes/:noteId", (req, res) => {
+  res.status(200).json({"message": "Retrieved note with ID: id_param"});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
